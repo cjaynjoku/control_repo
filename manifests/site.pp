22d3c484c2993eb2b79929071bf62c69632efeea
Januarius-Njoku <76001800+Januarius-Njoku@users.noreply.github.com>
2022-03-03 00:25:58 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,6 @@
 node default {
- file {'/root/README':
-  ensure => file,
-  content => 'this is a readme'
-}
-
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+  }
 }
